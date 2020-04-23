ee886f972b97a92254cdebda37dd0bb73bad7ccb
diskman88 <diskman88@hotmail.com>
2020-04-23 20:05:21 +0800

    新增板mpython-desktop-robot

---
+++ port/boards/mpython-desktop-robot/mpconfigboard.mk
@@ -0,0 +1,5 @@
+SDKCONFIG += boards/sdkconfig.base
+SDKCONFIG += boards/sdkconfig.240mhz
+SDKCONFIG += boards/sdkconfig.ble
+SDKCONFIG += boards/sdkconfig.spiram
+# SDKCONFIG += boards/mpython/sdkconfig.lwip
